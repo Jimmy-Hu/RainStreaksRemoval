22ba706a8ae92179aec42878fba66cf5348ef43d
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2025-12-09 18:41:20 +0800

    Update rainTrain4DArrayDataAll.m
    Update rainTrain4DArrayDataAll.m

--- src/Matlab/3d_DCT/Dictionary/rainTrain4DArrayDataAll.m
+++ src/Matlab/3d_DCT/Dictionary/rainTrain4DArrayDataAll.m
@@ -2,3 +2,4 @@ function [input, desiredOutput] = rainTrain4DArrayDataAll
 %RAINTRAIN4DARRAYDATA Load the rain images and clear images
 %   input       - Input data as an H-by-W-by-C-by-N array, where H is the
 %                 height and W is the width of the images, C is the number
+%                 of channels, and N is the number of images.
